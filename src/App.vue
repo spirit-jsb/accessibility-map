67bad8fc5350241bdd13a3f9c28cf969585b6986
jsb19930205 <sibo_jian_29903549@163.com>
2025-08-20 20:23:04 +0800

    feat: 添加无障碍地图相关SVG图标并更新应用界面
    refactor: 移除未使用的Vue示例组件和资源文件
    style: 更新应用样式和布局

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app-container">
+    <h1>无障碍地图网站</h1>
+    <p>欢迎使用无障碍地图应用</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.app-container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5f5f5;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+h1 {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+  color: #333;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+p {
+  font-size: 1.2rem;
+  color: #666;
 }
 </style>
