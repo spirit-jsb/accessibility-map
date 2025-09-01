f254244cb0c203b417a101f2f22e77d756680f88
jsb19930205 <jsb19930205@163.com>
2025-09-01 22:35:06 +0800

    build: 添加并配置 ESLint 导入相关插件
    添加 eslint-plugin-import 和 eslint-import-resolver-alias 依赖
    配置 ESLint 导入规则和别名解析，优化代码组织规范

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,63 @@
 import js from "@eslint/js";
-import globals from "globals";
-import pluginVue from "eslint-plugin-vue";
 import { defineConfig } from "eslint/config";
+import pluginImport from "eslint-plugin-import";
+import pluginVue from "eslint-plugin-vue";
+import globals from "globals";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,vue}"],
-    plugins: { js },
-    extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
+    plugins: {
+      import: pluginImport,
+    },
+    extends: [js.configs.recommended, ...pluginVue.configs["flat/essential"]],
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling", "index"],
+            "object",
+            "type",
+          ],
+          pathGroups: [
+            {
+              pattern: "vue",
+              group: "external",
+              position: "before",
+            },
+            {
+              pattern: "@/**",
+              group: "internal",
+            },
+            {
+              pattern: "@/components/**",
+              group: "internal",
+              position: "after",
+            },
+            {
+              pattern: "@/assets/**",
+              group: "internal",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+    },
+    settings: {
+      "import/resolver": {
+        alias: {
+          map: [["@", "./src"]],
+          extensions: [".js", ".jsx", ".ts", ".tsx", ".vue"],
+        },
+      },
+    },
   },
-  pluginVue.configs["flat/essential"],
 ]);
