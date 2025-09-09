b5b341b1de970eea8d47e2b7d9150468b7c8e8b9
jsb19930205 <jsb19930205@163.com>
2025-09-09 23:37:32 +0800

    refactor(LaunchView): 优化视图样式和布局并更新版权文本

--- src/views/LaunchView.vue
+++ src/views/LaunchView.vue
@@ -4,17 +4,17 @@ import logo from '../assets/images/logo.svg'
 
 <template>
   <div class="launch-view">
-    <div class="content-container-view">
-      <img :src="logo" alt="中国残疾人联合会" class="logo" />
+    <div class="navigation-bar"></div>
 
-      <div class="text-container-view">
-        <p class="small-text-item">无障碍设施建设问题</p>
-        <p class="medium-text-item">是一个国家和社会文明的标志</p>
-        <p class="large-text-item">我们要高度重视</p>
-      </div>
+    <img :src="logo" alt="中国残疾人联合会" class="logo-image" />
+
+    <div class="xis-quotations-section">
+      <p class="small-text">无障碍设施建设问题</p>
+      <p class="medium-text">是一个国家和社会文明的标志</p>
+      <p class="large-text">我们要高度重视</p>
     </div>
 
-    <p class="copyright-view">无障碍地图网站 © 2025</p>
+    <p class="copyright-text">Copyright © 2025 曦曦の聴感不良. 保留所有权利。</p>
   </div>
 </template>
 
@@ -28,50 +28,52 @@ import logo from '../assets/images/logo.svg'
   overflow: hidden;
 }
 
-.content-container-view {
+.navigation-bar {
   display: flex;
-  flex-direction: column;
+  position: relative;
   align-items: center;
+  z-index: 99;
+  background-color: transparent;
+  padding: 0;
+  height: 44px;
 }
 
-.logo {
-  margin: 40px 0px 0px;
+.logo-image {
+  margin: 12px auto;
   width: 68px;
   height: 68px;
 }
 
-.text-container-view {
-  margin: 48px 25px 0px 25px;
+.xis-quotations-section {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  margin: 36px 24px;
 }
 
-.small-text-item,
-.medium-text-item,
-.large-text-item {
-  margin: 0px;
+.small-text,
+.medium-text,
+.large-text {
+  margin: 0;
   color: #ffffff;
-  font-style: normal;
   font-weight: bold;
-  text-align: left;
 }
 
-.small-text-item {
-  margin-bottom: 6px;
+.small-text {
   font-size: 20px;
 }
 
-.medium-text-item {
-  margin-bottom: 5px;
+.medium-text {
   font-size: 25px;
 }
 
-.large-text-item {
+.large-text {
   font-size: 30px;
 }
 
-.copyright-view {
-  margin: auto auto calc(env(safe-area-inset-bottom, 34px) + 16px) auto;
+.copyright-text {
+  margin: auto auto calc(env(safe-area-inset-bottom, 34px) + 12px) auto;
   color: #ffffff;
-  font-style: normal;
   font-weight: bold;
   font-size: 14px;
   text-align: center;
