616f121906a26da8042e0184bb7bd5aff7bffc4f
jsb19930205 <jsb19930205@163.com>
2025-09-29 19:51:18 +0800

    build(vite): 根据环境模式配置不同的服务器和基础路径

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), basicSsl()],
-  server: {
-    https: true, // 启用 https
-    host: true, // 允许局域网访问
-  },
+export default defineConfig(({ mode }) => {
+  const config = {
+    plugins: [vue(), basicSsl()],
+  }
+
+  if (mode === 'development') {
+    config.server = {
+      https: true,
+      host: true,
+    }
+  } else if (mode === 'test') {
+    config.base = '/dev/accessibility-map/'
+  } else if (mode === 'production') {
+    config.base = '/accessibility-map/'
+  }
+
+  return config
 })
